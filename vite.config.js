590876c18b61017780b73c5163272e645be57bc2
Patryk Prezyna <patryk.prezyna@rommelag.com>
2023-03-09 17:08:13 +0100

    change config for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PatrykPrezyna/vue-playground/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/vue-playground/',
 })
